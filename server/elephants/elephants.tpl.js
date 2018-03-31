18f395bd1d9a51cb44261da7356df563226cb83a
Tobias Rös <roes@amicaldo.de>
2018-03-31 03:42:14 +0200

    remove css:zoom to enable cross brower compability

--- server/elephants/elephants.tpl.js
+++ server/elephants/elephants.tpl.js
@@ -6,7 +6,7 @@ templates['benjamin.hbs'] = template({"compiler":[7,">= 4.0.0"],"main":function(
     + alias4(((helper = (helper = helpers.speed || (depth0 != null ? depth0.speed : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"speed","hash":{},"data":data}) : helper)))
     + " ltr alive killable ka-crash\">\n    <img src=\""
     + alias4(((helper = (helper = helpers.eleFolder || (depth0 != null ? depth0.eleFolder : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"eleFolder","hash":{},"data":data}) : helper)))
-    + "benjamin.png\" alt=\"benjamin elephant\">\n</div>\n";
+    + "benjamin.png\" class=\"eleImg\" alt=\"benjamin elephant\">\n</div>\n";
 },"useData":true});
 templates['default.hbs'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
@@ -15,7 +15,7 @@ templates['default.hbs'] = template({"compiler":[7,">= 4.0.0"],"main":function(c
     + alias4(((helper = (helper = helpers.speed || (depth0 != null ? depth0.speed : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"speed","hash":{},"data":data}) : helper)))
     + " alive killable ltr ka-rebound\">\n    <img src=\""
     + alias4(((helper = (helper = helpers.eleFolder || (depth0 != null ? depth0.eleFolder : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"eleFolder","hash":{},"data":data}) : helper)))
-    + "default.gif\" alt=\"animated elephant\">\n</div>\n";
+    + "default.gif\" class=\"eleImg\" alt=\"animated elephant\">\n</div>\n";
 },"useData":true});
 templates['hunter.hbs'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=container.escapeExpression;
@@ -33,5 +33,5 @@ templates['lia.hbs'] = template({"compiler":[7,">= 4.0.0"],"main":function(conta
     + alias4(((helper = (helper = helpers.speed || (depth0 != null ? depth0.speed : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"speed","hash":{},"data":data}) : helper)))
     + " lia ltr alive killable ka-rotate\">\n    <img src=\""
     + alias4(((helper = (helper = helpers.eleFolder || (depth0 != null ? depth0.eleFolder : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"eleFolder","hash":{},"data":data}) : helper)))
-    + "lia.png\" alt=\"lia elephant\">\n</div>\n";
+    + "lia.png\" class=\"eleImg\" alt=\"lia elephant\">\n</div>\n";
 },"useData":true});
